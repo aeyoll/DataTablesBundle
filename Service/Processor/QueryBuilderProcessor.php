2db074f54fa6c18a556b3968903f53fe54501b03
shaggy <shaggy@none-server.(none)>
2014-04-24 23:22:14 -0700

    removing filtering from getTotalRecords

--- Service/Processor/QueryBuilderProcessor.php
+++ Service/Processor/QueryBuilderProcessor.php
@@ -290,7 +290,6 @@ class QueryBuilderProcessor extends AbstractProcessor implements ProcessorInterf
      */
     public function getTotalRecords(QueryBuilder $qb, $alias)
     {
-        $qb = $this->addSearch($qb);
         $qb->select(array("count({$alias})"))
             ->setMaxResults(1);
 
